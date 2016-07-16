db191d3081a9979cd7ee9dde2c4e8159e97055c6
sentraser95 <sentraser99@gmail.com>
2016-07-16 15:46:18 -0500

    manual para hacer la conexion de maquinas virtuales

--- createkmv.sh
+++ createkmv.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-virt-install --connect=qemu:///system --name=fedora --ram=256 --vcpus=1 --check-cpu --os-type=linux --hvm --vnc --accelerate --disk=/home/jaime/fedora.img,size=8,sparse=true --cdrom=/home/jaime/Downloads/Fedora-Workstation-Live-x86_64-24-1.2.iso 
+virt-install --connect=qemu:///system --name=fedora --ram=1024 --vcpus=1 --check-cpu --os-type=linux --hvm --vnc --accelerate --disk=/home/jaime/fedora.img,size=8,sparse=true --cdrom=/home/jaime/Downloads/CentOS-7-x86_64-Minimal-1511.iso 
